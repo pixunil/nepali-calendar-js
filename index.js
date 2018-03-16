6df8126f12cf2db974a869d167850c29f5c1ebb6
sg <sheetalgiri20@gmail.com>
2018-03-16 22:24:16 +0545

    functions added

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ module.exports =
   , isValidNepaliDate: isValidNepaliDate
   , isLeapNepaliYear: isLeapNepaliYear
   , nepaliMonthLength: nepaliMonthLength
-  , jalCal: jalCal
   , j2d: j2d
   , d2j: d2j
   , g2d: g2d
   , d2g: d2g
   }
 
+var nepCal = require('./nepali-calendar');
 /*
   Converts a Gregorian date to Nepali.
 */
@@ -37,7 +37,7 @@ function toGregorian(jy, jm, jd) {
   Checks whether a Nepali date is valid or not.
 */
 function isValidNepaliDate(jy, jm, jd) {
-  return  jy >= -61 && jy <= 3177 &&
+  return  jy >= 2000 && jy <= 2099 &&
           jm >= 1 && jm <= 12 &&
           jd >= 1 && jd <= nepaliMonthLength(jy, jm)
 }
@@ -46,88 +46,14 @@ function isValidNepaliDate(jy, jm, jd) {
   Is this a leap year or not?
 */
 function isLeapNepaliYear(jy) {
-  return jalCal(jy).leap === 0
+  return (nepCal.indexOf(jy)!=-1);
 }
 
 /*
   Number of days in a given month in a Nepali year.
 */
 function nepaliMonthLength(jy, jm) {
-  if (jm <= 6) return 31
-  if (jm <= 11) return 30
-  if (isLeapNepaliYear(jy)) return 30
-  return 29
-}
-
-/*
-  This function determines if the Nepali (Persian) year is
-  leap (366-day long) or is the common year (365 days), and
-  finds the day in March (Gregorian calendar) of the first
-  day of the Nepali year (jy).
-
-  @param jy Nepali calendar year (-61 to 3177)
-  @return
-    leap: number of years since the last leap year (0 to 4)
-    gy: Gregorian year of the beginning of Nepali year
-    march: the March day of Farvardin the 1st (1st day of jy)
-  @see: http://www.astro.uni.torun.pl/~kb/Papers/EMP/PersianC-EMP.htm
-  @see: http://www.fourmilab.ch/documents/calendar/
-*/
-function jalCal(jy) {
-  // Nepali years starting the 33-year rule.
-  var breaks =  [ -61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
-                , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
-                ]
-    , bl = breaks.length
-    , gy = jy + 621
-    , leapJ = -14
-    , jp = breaks[0]
-    , jm
-    , jump
-    , leap
-    , leapG
-    , march
-    , n
-    , i
-
-  if (jy < jp || jy >= breaks[bl - 1])
-    throw new Error('Invalid Nepali year ' + jy)
-
-  // Find the limiting years for the Nepali year jy.
-  for (i = 1; i < bl; i += 1) {
-    jm = breaks[i]
-    jump = jm - jp
-    if (jy < jm)
-      break
-    leapJ = leapJ + div(jump, 33) * 8 + div(mod(jump, 33), 4)
-    jp = jm
-  }
-  n = jy - jp
-
-  // Find the number of leap years from AD 621 to the beginning
-  // of the current Nepali year in the Persian calendar.
-  leapJ = leapJ + div(n, 33) * 8 + div(mod(n, 33) + 3, 4)
-  if (mod(jump, 33) === 4 && jump - n === 4)
-    leapJ += 1
-
-  // And the same in the Gregorian calendar (until the year gy).
-  leapG = div(gy, 4) - div((div(gy, 100) + 1) * 3, 4) - 150
-
-  // Determine the Gregorian date of Farvardin the 1st.
-  march = 20 + leapJ - leapG
-
-  // Find how many years have passed since the last leap year.
-  if (jump - n < 6)
-    n = n - jump + div(jump + 4, 33) * 33
-  leap = mod(mod(n + 1, 33) - 1, 4)
-  if (leap === -1) {
-    leap = 4
-  }
-
-  return  { leap: leap
-          , gy: gy
-          , march: march
-          }
+  return nepCal[jy][jm];
 }
 
 /*
@@ -139,10 +65,23 @@ function jalCal(jy) {
   @return Julian Day number
 */
 function j2d(jy, jm, jd) {
-  var r = jalCal(jy)
-  return g2d(r.gy, 3, r.march) + (jm - 1) * 31 - div(jm, 7) * (jm - 7) + jd - 1
+  i=ly=nepCal.startYear;
+  d=nepCal.startJulianDay-1;
+  ly=nepCal.leapYears[0];
+  for(j=1;j<nepCal.leapYears.length;j++){
+    if(ly>jy) break;
+    d+=(ly-i)*365;
+    d+=366;
+    i=ly+1;
+    ly=nepCal.leapYears[j]
+  }
+  if(jy-i > 1) d+=(jy-i)*365;
+  for(i=1;i<jm;i++){
+    d+=nepCal[jy][i-1];
+  }
+  d+=jd;
+  return d;
 }
-
 /*
   Converts the Julian Day number to a date in the Nepali calendar.
 
@@ -153,44 +92,33 @@ function j2d(jy, jm, jd) {
     jd: Nepali day (1 to 29/31)
 */
 function d2j(jdn) {
-  var gy = d2g(jdn).gy // Calculate Gregorian year (gy).
-    , jy = gy - 621
-    , r = jalCal(jy)
-    , jdn1f = g2d(gy, 3, r.march)
-    , jd
-    , jm
-    , k
-
-  // Find number of days that passed since 1 Farvardin.
-  k = jdn - jdn1f
-  if (k >= 0) {
-    if (k <= 185) {
-      // The first 6 months.
-      jm = 1 + div(k, 31)
-      jd = mod(k, 31) + 1
-      return  { jy: jy
-              , jm: jm
-              , jd: jd
-              }
-    } else {
-      // The remaining months.
-      k -= 186
-    }
-  } else {
-    // Previous Nepali year.
-    jy -= 1
-    k += 179
-    if (r.leap === 1)
-      k += 1
+  jdn=jdn-nepCal.startJulianDay-1+2;
+  jy=ly=nepCal.startYear;
+  d=td=jdn;
+  for(i=0;i<nepCal.leapYears.length;i++){
+    td-= (nepCal.leapYears[i]-jy)*365;
+    td-= 366;
+    if(td<0) break;
+    d=td;
+    jy = nepCal.leapYears[i]+1;
   }
-  jm = 7 + div(k, 30)
-  jd = mod(k, 30) + 1
+  while(d > 365){
+    d-=365;
+    jy++;
+  }
+  for(jm=1;jm<12;jm++){
+    if(d>nepCal[jy][jm])
+      d-=nepCal[jy][jm-1];
+    else
+      break;
+  }
+  jd=d;
+
   return  { jy: jy
           , jm: jm
           , jd: jd
           }
 }
-
 /*
   Calculates the Julian Day number from Gregorian or Julian
   calendar dates. This integer number corresponds to the noon of
